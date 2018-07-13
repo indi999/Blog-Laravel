96f0c376f281e1d3db3d2d2204cf6c6f5826de56
Nenad Milenkovic <milenkovicnkv@gmail.com>
2018-07-13 11:41:08 +0000

    add file Install laravel procedure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Nenad Milenkovic
+                    Blog
                 </div>
 
                 <div class="links">
